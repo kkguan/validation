2e8af068344d5b5e820f4f7589b2c6a211c39ef1
twosee <twose@qq.com>
2021-09-22 18:33:04 +0800

    Add ValidationErrorDumper

---
+++ src/ValidationErrorDumper.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace KK\Validation;
+
+use function implode;
+
+class ValidationErrorDumper
+{
+    public static function dump(array $errors): string
+    {
+        $messages = [];
+        foreach ($errors as $key => $errorSet) {
+            $messages[] = "Attribute '{$key}' violates the following rules: " . implode(', ', $errorSet);
+        }
+
+        return implode("\n", $messages);
+    }
+}
